522b592b56fc1e0ea6ceca13c2c305f55a9cbbdb
wangtingzhao <w1615216175@163.com>
2018-03-13 23:03:55 +0800

    模板编写

--- test项目/pages/abouts/abouts.js
+++ test项目/pages/abouts/abouts.js
@@ -4,5 +4,31 @@ Page({
     },
     onLoad: function () {
       
+    },
+    open_img: function(){
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var tempFilePaths = res.tempFilePaths
+        }
+      })
+    },
+    Release: function() {
+      wx.request({
+        url: 'test.php', //仅为示例，并非真实的接口地址
+        data: {
+           x: '' ,
+           y: ''
+        },
+        header: {
+            'content-type': 'application/json' // 默认值
+        },
+        success: function(res) {
+          console.log(res.data)
+        }
+      })
     }
   })
--- test项目/pages/order/order.js
+++ test项目/pages/order/order.js
@@ -1,8 +1,21 @@
 Page({
     data: {
-      logs: []
+      num: 0,
     },
     onLoad: function () {
       
+    },
+    tabaja: function(num){
+      if(num == 0){
+        
+      }else if(num == 1){
+
+      }else if(num == 2){
+
+      }else if(num == 3){
+
+      }else{
+        
+      }
     }
   })
